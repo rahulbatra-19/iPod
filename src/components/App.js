2ef7d2dbaa2e486372dc26a7bac91ef592d15903
rahulbatra-19 <batrarahul409@example.com>
2023-08-22 19:31:53 +0530

    Changed directory structure

---
+++ src/components/App.js
@@ -0,0 +1,146 @@
+import React from "react";
+import ".styles/App.css";
+import Controls from "./Controls";
+import Screen from "./Screen";
+import ZingTouch from "zingtouch";
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      menuItems: [
+        {
+          id: 1,
+          title: "CoverFlow",
+        },
+        {
+          id: 2,
+          title: "Music",
+        },
+        {
+          id: 3,
+          title: "Games",
+        },
+        {
+          id: 4,
+          title: "Settings",
+        },
+      ],
+      isLeftVisible: false,
+      onscreen: "",
+    };
+    var screenmain = "";
+  }
+  // this is to handing the color of items
+  handleColor = () => {
+    let div = document.getElementById("CoverFlow");
+    if (div) {
+      div.style.backgroundColor = "whitesmoke";
+      div.style.color = "black";
+    }
+
+    div = document.getElementById("Music");
+    if (div) {
+      div.style.backgroundColor = "whitesmoke";
+      div.style.color = "black";
+    }
+    div = document.getElementById("Games");
+    if (div) {
+      div.style.backgroundColor = "whitesmoke";
+      div.style.color = "black";
+    }
+    div = document.getElementById("Settings");
+    if (div) {
+      div.style.backgroundColor = "whitesmoke";
+      div.style.color = "black";
+    }
+  };
+  // this is a function of Selecting the menu items
+  handleControlClick = () => {
+    let angle = 0;
+    // this is to select the target element and using Zing touch on it for selecting the component
+    const target = document.getElementById("menu");
+    const region = new ZingTouch.Region(target);
+    // binding target to the rotate
+    region.bind(target, "rotate", (e) => {
+      angle = angle + e.detail.distanceFromLast;
+      // this is for hovering over items and focus goes on items and changing color of background
+      if (Math.abs(angle) > 0 && Math.abs(angle) < 20) {
+        this.handleColor();
+        let curEle = document.getElementById("CoverFlow");
+        if (curEle) {
+          curEle.style.backgroundColor = "#0099FF";
+          curEle.style.color = "white";
+        }
+        this.screenmain = "CoverFlow";
+      }
+      if (Math.abs(angle) > 15 && Math.abs(angle) < 35) {
+        this.handleColor();
+        let curEle = document.getElementById("Music");
+        if (curEle) {
+          curEle.style.backgroundColor = "#0099FF";
+          curEle.style.color = "white";
+        }
+        this.screenmain = "Music";
+      }
+      if (Math.abs(angle) > 30 && Math.abs(angle) < 50) {
+        this.handleColor();
+        let curEle = document.getElementById("Games");
+
+        if (curEle) {
+          curEle.style.backgroundColor = "#0099FF";
+          curEle.style.color = "white";
+        }
+        this.screenmain = "Games";
+      }
+      if (Math.abs(angle) >= 45 && Math.abs(angle) < 65) {
+        this.handleColor();
+        let curEle = document.getElementById("Settings");
+
+        if (curEle) {
+          curEle.style.backgroundColor = "#0099FF";
+          curEle.style.color = "white";
+        }
+        this.screenmain = "Settings";
+      }
+
+      if (Math.abs(angle) > 65) {
+        angle = 0;
+      }
+    });
+  };
+  // this is the function for the menu button to show the menu of the component on screen
+  handleScreenToggle = () => {
+    this.setState((prevState) => ({
+      isLeftVisible: !prevState.isLeftVisible,
+    }));
+  };
+  // this is the function for the main middle button to select the screen component
+  handleSelectedItem = () => {
+    this.setState({
+      onscreen: this.screenmain,
+    });
+  };
+
+  render() {
+    const { menuItems, isLeftVisible, onscreen } = this.state;
+    return (
+      <div className="App">
+        {/* this is the Screen part of the Ipod */}
+        <Screen
+          menuItems={menuItems}
+          isLeftVisible={isLeftVisible}
+          onscreen={onscreen}
+        />
+        {/* this the controller part of the Ipod */}
+        <Controls
+          onScreenToggle={this.handleScreenToggle}
+          onControlClick={this.handleControlClick}
+          selectedItem={this.handleSelectedItem}
+        />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/Screen.js
@@ -0,0 +1,103 @@
+import React from "react";
+import ".styles/Screen.css";
+
+class Screen extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      // variable for left menu is visible
+      isLeftVisible: props.isLeftVisible,
+      // variable if any item is selected
+      onscreen: props.onscreen,
+    };
+  }
+  componentDidUpdate(prevProps) {
+    // Check if the onscreen prop is  changed
+    if (prevProps.onscreen !== this.props.onscreen) {
+      this.setState({
+        isLeftVisible: !this.state.isLeftVisible,
+        onscreen: this.props.onscreen,
+      });
+    }
+    // Check if the isLeftVisible prop has changed
+
+    if (prevProps.isLeftVisible !== this.props.isLeftVisible) {
+      this.setState({
+        isLeftVisible: !this.state.isLeftVisible,
+        onscreen: "",
+      });
+    }
+  }
+  render() {
+    const { menuItems } = this.props;
+    const { isLeftVisible, onscreen } = this.state;
+    return (
+      // this is the main screen div
+      <div className="main">
+        {isLeftVisible ? (
+          // this is for left side of the screen to show the items of component
+          <div className="left">
+            <p>iPod.js</p>
+            <ul>
+              {menuItems.map((item) => (
+                <li key={item.id} id={item.title}>
+                  <span className="text">{item.title}</span>
+                  <span className="symbol">></span>
+                </li>
+              ))}
+            </ul>
+          </div>
+        ) : (
+          // this is if the component is selected
+          <div className="full">
+            {/* if CoverFlow is selected */}
+            {onscreen === "CoverFlow" && (
+              <div className="full-func">
+                <span>CoverFlow</span>
+              </div>
+            )}
+            {/* if Music is selected */}
+            {onscreen === "Music" && (
+              <div className="full-func">
+                <p id="music">
+                  <img src="https://images.genius.com/cbad795cdb3843247073fac8af76b117.1000x1000x1.png"></img>
+                  <span>
+                    Sicko Mode <br></br>
+                    Travis Scott
+                  </span>
+                </p>
+                <div id="music-timeline">
+                  0<div id="music-time"></div>
+                  4:00
+                </div>
+              </div>
+            )}
+            {/* if Games item  is selected */}
+
+            {onscreen === "Games" && (
+              <div className="full-func">
+                <img
+                  src="https://toppng.com/uploads/preview/dice-115628871397usecbjyyw.png"
+                  alt="games"
+                ></img>
+                <p>Games</p>
+              </div>
+            )}
+            {/* if Setting item is selected */}
+            {onscreen === "Settings" && (
+              <div className="full-func">
+                <img
+                  src="https://e7.pngegg.com/pngimages/516/751/png-clipart-computer-icons-setting-windows-настройки.png"
+                  alt="Settings"
+                ></img>
+                <p>Settings</p>
+              </div>
+            )}
+          </div>
+        )}
+      </div>
+    );
+  }
+}
+
+export default Screen;
